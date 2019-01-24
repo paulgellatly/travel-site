03141bfa2fe266290022cadb4f9abc0e1a62a1d7
Paul <paulgellatly@icloud.com>
2019-01-24 15:27:03 +0000

    stop gap started mobileMenu functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,21 +1,4 @@
-var $ = require('jquery');
-//var Person = require('./modules/Person');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-    payTaxes() {
-        console.log(this.name + " now owes $100 in taxes.");
-    }
-}
-
-alert("Testing 5435356");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Doe", "orange");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
 
---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+
+class MobileMenu {
+    constructor() {
+
+    }
+}
+
+export default MobileMenu;
